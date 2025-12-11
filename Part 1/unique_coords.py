ed4961077ec08fb179391edea2fed4705b610cfe
Liam <134171827+FieldFlash@users.noreply.github.com>
2025-12-11 15:33:04 -0500

    added unique_coords.py

---
+++ Part 1/unique_coords.py
@@ -0,0 +1,21 @@
+def unique_coords(coords):
+    """
+    Problem 3 — Unique Coordinates
+    You are given a list of coordinate pairs (tuples), possibly with duplicates.
+    Write a function unique_coords(coords) that:
+    ● takes a list like [(1,2), (3,4), (1,2), (3,4), (5,6)]
+    ● returns a set of all unique coordinates.
+    Example:
+    unique_coords([(1,2), (3,4), (3,4), (1,2)]) → {(1,2), (3,4)}
+    """
+    uniqueSet = set ()
+    for i in range(len(coords)):
+        if coords[i] not in uniqueSet:
+            uniqueSet.add(coords[i])
+    return uniqueSet
+
+def main():
+    coords = [(1,2), (3,4), (3,4), (1,2)]
+    print(unique_coords(coords))
+
+main()
