99e3aff10e885ea788c63991aac1d91970a5e061
Liam <134171827+FieldFlash@users.noreply.github.com>
2025-12-11 16:32:07 -0500

    added file: Part 1/running_total.py

---
+++ Part 1/running_total.py
@@ -0,0 +1,11 @@
+def running_total(nums):
+    """
+    Problem 5 — Generator of Running Totals
+    Write a generator function running_total(nums) that:
+    ● yields the cumulative total as you iterate through the list
+    Example:
+    g = running_total([2, 5, 3])
+    next(g) → 2
+    next(g) → 7
+    next(g) → 10
+    """
